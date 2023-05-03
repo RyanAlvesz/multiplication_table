a1671767b7924d28767d7302d7b18ca0790aeb16
Ryan Alves <ra546038@gmail.com>
2023-05-03 09:29:41 -0300

    Finished Project

---
+++ src/br/senai/sp/jandira/Tabuada.java
@@ -0,0 +1,48 @@
+package br.senai.sp.jandira;
+
+import java.util.Scanner;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+	
+		Scanner teclado = new Scanner(System.in);
+		
+		int multiplicando, multiplicadorMenor, multiplicadorMaior, maior, menor, tabuada;
+		
+		System.out.println("**************************");
+		System.out.println("         TABUADA");
+		System.out.println("**************************");
+		System.out.println("");
+		
+		System.out.println("Qual o multiplicando?");
+		multiplicando = teclado.nextInt();
+		
+		System.out.println("Qual o mínimo multiplicador?");
+		multiplicadorMenor = teclado.nextInt();
+		
+		System.out.println("Qual o maior multiplicador?");
+		multiplicadorMaior = teclado.nextInt();
+		
+
+		if (multiplicadorMenor < multiplicadorMaior) {
+			maior = multiplicadorMaior;
+			menor = multiplicadorMenor;
+		} else {
+			maior = multiplicadorMenor;
+			menor = multiplicadorMaior;
+		}
+		
+		while(maior > menor) {
+			
+			tabuada = multiplicando * menor;
+			System.out.println(tabuada);
+			menor++;
+			
+		}
+		
+		System.out.println(multiplicando * maior);
+		
+	}
+
+}
